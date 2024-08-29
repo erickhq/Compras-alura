d715a03032e48f42d9381bac55d39f57bb2a6e19
Erick Oliveira <erick.oliveira.eu@gmail.com>
2024-08-29 20:38:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,15 @@ botaoSalvarItem.addEventListener("click", adicionarItem);
 
 function adicionarItem(evento) {
     evento.preventDefault()
-    console.log("entrei na função")
+
+    const itemDaLista = document.createElement("li");
+    const containerItemLista = document.createElement("div");
+    containerItemLista.classList.add("lista-item-container");
+
+    const containerNomeDoItem = document.createElement("div");
+    const nomeDoItem = document.createElement("p");
+    nomeDoItem.innerText = item.value;
+    containerNomeDoItem.appendChild(nomeDoItem);
+
+    itemDaLista.appendChild(containerItemLista);
 }
